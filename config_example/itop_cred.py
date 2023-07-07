a77cc1b386e57cb59c5151690a36bbdf2bdb7440
MartinLuzifer <kanzlur.g@gmail.com>
2023-07-07 15:45:55 +0500

    rename_folder_example2

---
+++ config_example/itop_cred.py
@@ -0,0 +1,6 @@
+ITOP_URL = ''
+ITOP_LOGIN = ''
+ITOP_PASSWD = ''
+ITOP_HEADER = {  # FOR API ACCESS
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
+}
---
+++ config_example/mongodb.py
@@ -0,0 +1,5 @@
+mdb_HOST = 'mongo'  # == Имя сервиса mongodb или имя контейнера
+mdb_PORT = 27017
+mdb_USERNAME = ''
+mdb_PASSWORD = ''
+mdb_DBNAME = 'aiogram_fsm'
