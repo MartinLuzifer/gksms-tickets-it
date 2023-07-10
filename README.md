9952e4bb4ff30132ee0677badfddc2afe9c48a4a
MartinLuzifer <kanzlur.g@gmail.com>
2023-07-10 09:18:30 +0500

    change_manual_and_bot_sh

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
 # gksms-tickets-it
 
-# Настройка:
 
-1. Переименовать каталог `config_example` в `config`  
-2. В файле `config/tgb_token.py` указать токен вашего бота
-3. В файле `config/itop_cred.py` указать логин и пароль от сервера itop
-4. В файле `config/mongodb.py` указать логин и пароль от субд
-   - Указать такой же пароль в файле `docker-compose.yml`)
-   ```yaml
-   ...    
-   environment:
-     MONGO_INITDB_ROOT_USERNAME: root
-     MONGO_INITDB_ROOT_PASSWORD: gfWUIR
-   ...
-   ```
 
    
-# Запуск
+# Установка:
 
 1. Установить необходимые компоненты
 
@@ -27,6 +14,37 @@
 
        sudo git clone https://github.com/MartinLuzifer/gksms-tickets-it.git
 
-3. Запустить бота и mongodb
+# Настройка:
+
+1. Переименовать каталог `config_example` в `config`  
+2. В файле `config/tgb_token.py` указать токен вашего бота
+3. В файле `config/itop_cred.py` указать логин и пароль от сервера itop
+4. В файле `config/mongodb.py` указать логин и пароль от СУБД
+
+   - Указать такой же пароль в файле `docker-compose.yml`)
+   ```yaml
+   ...    
+   environment:
+     MONGO_INITDB_ROOT_USERNAME: root
+     MONGO_INITDB_ROOT_PASSWORD: gfWUIR
+   ...
+   ```
+
+# Запуск:
+
+1. Запустить бота и mongodb
 
        sudo docker-compose up -d
+
+2. Проверить работоспособность
+
+# Альтернативный способ запуска
+
+
+1. Использовать bash-скрипт для запуска
+
+       ./bot.sh start
+
+2. Использовать bash-скрипт для остановки (будет удален контейнер и образ контейнера)
+
+       ./bot.sh stop
--- bot.sh
+++ bot.sh
@@ -1,28 +1,42 @@
 #!/usr/bin/bash
 
-export tgb_image_name="tgb-gksms-dev-tickets-it"
+export red='\033[0;31m'
+export green='\033[1;36m'
+export nc='\033[0m'
+export tgb_image_name='tgb-gksms-dev-tickets-it'
 export tgb_container_name='tgb'
-export args_not_found="ARGS NOT FOUND, USED:
-                START BOT ----> ./bot.sh start
-                STOP BOT -----> ./bot.sh stop
-                GET HELP -----> ./bot.sh help
+export args_not_found="
+${red}ARGS NOT FOUND.${nc}
 
-                NOT RUN sh bot.sh start OR bash bot.sh start"
+${green}USED:${nc}
+${green}START${nc} BOT----> ./bot.sh ${green}start${nc}
+${green}STOP${nc} BOT -----> ./bot.sh ${green}stop${nc}
+GET ${green}HELP${nc} -----> ./bot.sh ${green}help${nc}
+
+${red}
+!!! NOT RUN sh bot.sh start
+!!! NOT RUN bash bot.sh start.
+!!! ONLY ./bot.sh
+${nc}
+"
 
 if [ "$1" == 'start' ]; then
 
-  echo "$1"
-  sudo docker build -t $tgb_image_name . && sudo docker run -d --name $tgb_container_name $tgb_image_name
+  echo "$1" && \
+  sudo docker build -t $tgb_image_name . && \
+  sudo docker run -d --name $tgb_container_name $tgb_image_name
 
 elif [ "$1" == 'stop' ]; then
 
-  echo "$1"
-  sudo docker container stop $tgb_container_name && sudo docker container rm $tgb_container_name && sudo docker image rm $tgb_image_name
+  echo "$1" && \
+  sudo docker container stop $tgb_container_name && \
+  sudo docker container rm $tgb_container_name && \
+  sudo docker image rm $tgb_image_name
 
 elif [ -n "$1" -o ! "$1" ]; then
-  echo "$args_not_found"
+  echo -e "$args_not_found"
 
 elif [ "$1" == "help" ]; then
-  echo "$args_not_found"
+  echo -e "$args_not_found"
 
 fi
